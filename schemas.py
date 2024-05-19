b4ec9df67e14ed70846bac9db727b0a4810fdaba
LeonelC5 <112716144+LeonelC5@users.noreply.github.com>
2024-05-18 23:59:54 -0500

    Delete schemas.py

--- schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-class Item(BaseModel):
-    nombre: str
-    apellido: str
-    correo: str
-    password: str
-    celular: str
-    description: str
-    imagen: str
