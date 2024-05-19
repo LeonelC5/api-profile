845d985ef9a6f0cc7596d233c065a66e9cffd5cf
LeonelC5 <112716144+LeonelC5@users.noreply.github.com>
2024-05-18 21:52:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,51 +16,51 @@ database_name = os.environ.get("db_profile")
 def get_profiles():
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)  
     cursor = mydb.cursor()
-    cursor.execute("SELECT * FROM profile")
+    cursor.execute("SELECT * FROM user")
     result = cursor.fetchall()
     mydb.close()
-    return {"profiles": result}
+    return {"users": result}
 
 # Get a profile by ID
 @app.get("/profile/{id}")
 def get_profile(id: int):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)  
     cursor = mydb.cursor()
-    cursor.execute(f"SELECT * FROM profile WHERE id = {id}")
+    cursor.execute(f"SELECT * FROM user WHERE id = {id}")
     result = cursor.fetchone()
     mydb.close()
-    return {"profile": result}
+    return {"user": result}
 
 # Add a new profile
 @app.post("/profile")
 def add_profile(item:schemas.Item):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)  
     cursor = mydb.cursor()
-    sql = "INSERT INTO profiles (nombre, apellido, correo, password, celular, description, imagen) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO user (nombre, apellido, correo, password, celular, description, imagen) VALUES (%s, %s, %s, %s, %s, %s, %s)"
     val = (item.nombre, item.apellido, item.correo, item.password, item.celular, item.description, item.imagen)
     cursor.execute(sql, val)
     mydb.commit()
     mydb.close()
-    return {"message": "profile added successfully"}
+    return {"message": "user added successfully"}
 
 # Modify a profile
 @app.put("/profile/{id}")
 def update_profile(id:int, item:schemas.Item):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)  
     cursor = mydb.cursor()
-    sql = "UPDATE profiles set nombre=%s, apellido=%s,correo=%s, password=%s, celular=%s, description=%s, imagen=%s  where id=%s"
+    sql = "UPDATE user set nombre=%s, apellido=%s,correo=%s, password=%s, celular=%s, description=%s, imagen=%s  where id=%s"
     val = (item.nombre, item.apellido, item.correo, item.password, item.celular, item.description, item.imagen, id)
     cursor.execute(sql, val)
     mydb.commit()
     mydb.close()
-    return {"message": "profile modified successfully"}
+    return {"message": "user modified successfully"}
 
 # Delete a profile by ID
 @app.delete("/profile/{id}")
 def delete_profile(id: int):
     mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)  
     cursor = mydb.cursor()
-    cursor.execute(f"DELETE FROM profile WHERE id = {id}")
+    cursor.execute(f"DELETE FROM user WHERE id = {id}")
     mydb.commit()
     mydb.close()
     return {"message": "profile deleted successfully"}
